cd62fc66a638148dffe8a770937a25f8066ff982
adnan-jahbli <adnan.jahbli@gmail.com>
2023-03-17 15:47:23 -0700

    The 8 task

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char hex[] = "0123456789abcdef";
+	int hex_size = sizeof(hex) / sizeof(hex[0]);
+
+	for (i = 0; i < hex_size; i++)
+		putchar(hex[i]);
+	putchar('\n');
+	return (0);
+}
