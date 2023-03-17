8ee09f49970d313db57ac83aa0a6451dccc3cd18
adnan-jahbli <adnan.jahbli@gmail.com>
2023-03-16 19:43:56 -0700

    fix bug

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,8 @@
  */
 int main(void)
 {
-	fprintf(stderr, "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char a[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fputs(a, stderr);
 	return (1);
 }
