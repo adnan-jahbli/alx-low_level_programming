1a1f7f9c183c438d308aaa20301402bcb3007ce0
adnan-jahbli <adnan.jahbli@gmail.com>
2023-03-21 13:15:16 -0700

    Adding the 3-islower.C

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,3 +10,6 @@ void print_alphabet(void);
 
 /* Declaration of print_alphabet function which is defined in 2-print_alphabet_x10.c*/
 void print_alphabet_x10(void);
+
+/* Declaration of _islower function which is defined in 3-islower.c*/
+int _islower(int c);
