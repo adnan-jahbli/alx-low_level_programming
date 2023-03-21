6dbaf375a7fd1d3a77bca727754bd50f29d258fc
adnan-jahbli <adnan.jahbli@gmail.com>
2023-03-21 12:16:41 -0700

    updating 1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -11,17 +11,3 @@ int main(void)
 	print_alphabet();
 	return (0);
 }
-
-/**
- * print_alphabet - Priting the alphabets in lowercase, followed by a new line
- *
- * Return: void
- */
-void print_alphabet(void)
-{
-	int i;
-
-	for (i = 'a'; i <= 'z'; i++)
-		_putchar(i);
-	_putchar('\n');
-}
