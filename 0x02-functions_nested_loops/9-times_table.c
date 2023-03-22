9573f468390398d862107ad49c79c32982eefc9a
adnan-jahbli <adnan.jahbli@gmail.com>
2023-03-21 19:25:05 -0700

    Updating 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,9 +15,9 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			k = i * j;
-			if (k < 10)
+			if (k < 10 && j != 0)
 				_putchar(' ');
-			else
+			else if (k >= 10 && j != 0)
 				_putchar((k / 10) + '0');
 			_putchar((k % 10) + '0');
 			if (j == 9)
