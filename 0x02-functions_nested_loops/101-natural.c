a6336ff484e832548baac23d6d8aa908ff522217
adnan-jahbli <adnan.jahbli@gmail.com>
2023-03-22 18:20:13 -0700

    Adding the 101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/* Prorotype of muliple_sum function */
+int multiples_sum(int n);
+
+/**
+ * main - prints the return value of multiple_sum function
+ *
+ * Return: 0 (Success).
+ */
+int main(void)
+{
+	int a;
+
+	a = 1024;
+	printf("sum = %d\n", multiples_sum(a));
+	return (0);
+}
+
+/**
+ * multiples_sum - computes and prints the sum of all the multiples
+ * of 3 or 5 below n (excluded), followed by a new line.
+ *
+ * @n: the exluded number
+ * Return: the result.
+ */
+int multiples_sum(int n)
+{
+	int i, sum;
+
+	sum = 0;
+	for (i = 3; i < n; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+		else
+			continue;
+	}
+	return (sum);
+}
+
