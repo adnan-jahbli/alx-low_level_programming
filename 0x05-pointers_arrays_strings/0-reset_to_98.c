4ceb63b1483c8bf0c29eeabce1729712edf0c1eb
adnan-jahbli <adnan.jahbli@gmail.com>
2023-03-27 09:23:35 -0700

    Addint README, main.h and 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset the value of a given variable to 98.
+ * @n: a pointer variable that changes the value of the variable it points to.
+ * Return: void.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory was created to practice pointers, arrays and strings in C programming.
