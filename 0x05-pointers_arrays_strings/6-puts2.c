56fbab2046493736d545193b592ebc7c132272e0
adnan-jahbli <adnan.jahbli@gmail.com>
2023-03-27 13:52:53 -0700

    Adding the 6-puts.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string,
+ * starting with the first character, followed by a new line.
+ * @str: the given string to reverse.
+ * Return: void.
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0 || i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
