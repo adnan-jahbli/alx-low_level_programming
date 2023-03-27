77b514cf62004d14c198871d8868ac6b20c197d6
adnan-jahbli <adnan.jahbli@gmail.com>
2023-03-27 10:59:58 -0700

    Bug fixed in 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,12 +7,12 @@
  */
 void print_rev(char *s)
 {
-	int i, length;
+	int i, j, length;
 
 	length = 0;
 	for (i = 0; s[i] != '\0'; i++)
 		length = i + 1;
-	for (i = length; i >= 0; i--)
-		_putchar(s[i]);
+	for (j = length - 1; j >= 0; j--)
+		_putchar(s[j]);
 	_putchar('\n');
 }
