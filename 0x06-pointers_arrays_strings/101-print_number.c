b3465b3a500ec2ec582aa6d38480bd0aed2d84c5
adnan-jahbli <adnan.jahbli@gmail.com>
2023-03-30 10:21:33 -0700

    Adding the file 101

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_number - a function that prints an integer.
+ * @n: the given number
+ * Return: void.
+ */
+void print_number(int n)
+{
+	int i, j, k;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	i = 1;
+	k = n;
+	while (k > 9)
+	{
+		k /= 10;
+		i *= 10;
+	}
+	for (j = i; j >= 1; j /= 10)
+	{
+		_putchar((n / j) % 10 + '0');
+	}
+}
