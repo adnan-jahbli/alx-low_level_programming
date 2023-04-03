be16e3f6b6427484a2ed8530b6da84645e1bffeb
adnan-jahbli <adnan.jahbli@gmail.com>
2023-04-03 17:41:53 +0000

    Adding 1-memcpy.c and updating main.h

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - a function that copies memory area.
+ * @dest: the buffer where the copied memory area will be passed to
+ * @src: the copied memory area
+ * @n: number of elements to be copied from src
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+
+	return (dest);
+}
