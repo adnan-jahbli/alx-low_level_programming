cd7b348aee38ea1ed78afb84688bd034803db2fb
adnan-jahbli <adnan.jahbli@gmail.com>
2023-04-04 21:39:50 +0000

    updating the file 7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strstr - a function that locates a substring
+ * print_chessboard - a function that prints the chessboard
  * @a: the given string to print
  * return: void.
  */
@@ -14,7 +14,7 @@ void print_chessboard(char (*a)[8])
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
-		{	
+		{
 			_putchar(a[i][j]);
 			if (j == 7)
 				_putchar('\n');
