6986e14b82210ea7b92b27c337883d18f3878fb8
adnan-jahbli <adnan.jahbli@gmail.com>
2023-04-18 01:15:29 +0000

    updating file 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,13 +14,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	d = malloc(sizeof(dog_t));
 	if (!d || !name || !owner)
-		return (NULL);
-
-	if (d)
 	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
+		free(d);
+		return (NULL);
 	}
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
 	return (d);
 }
