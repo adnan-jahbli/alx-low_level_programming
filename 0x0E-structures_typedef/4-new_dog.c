0e3e4ee3073ab7f5dae6d162eb72a93887d6b0c2
adnan-jahbli <adnan.jahbli@gmail.com>
2023-04-18 01:32:24 +0000

    updating file 4 and fixing the bug

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,6 +10,7 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i, j, counter;
 	dog_t *d;
 
 	d = (dog_t *) malloc(sizeof(dog_t));
@@ -18,10 +19,32 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(d);
 		return (NULL);
 	}
+	for (i = 0; name[i]; i++)
+		;
 
-	d->name = name;
+	for (j = 0; owner[j]; j++)
+		;
+
+	d->name = malloc(i + 1);
+	d->owner = malloc(j + 1);
+
+	if (!(d->name) || !(d->owner))
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+
+	for (counter = 0; counter < i; counter++)
+		d->name[counter] = name[counter];
+	d->name[counter] = '\0';
+
 	d->age = age;
-	d->owner = owner;
+
+	for (counter = 0; counter < j; counter++)
+		d->owner[counter] = owner[counter];
+	d->owner[counter] = '\0';
 
 	return (d);
 }
