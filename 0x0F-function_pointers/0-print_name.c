3141e484f98cdcb4a44238857fdef2cbae95b80e
adnan-jahbli <adnan.jahbli@gmail.com>
2023-04-19 21:34:15 +0000

    creating readme, header file and file 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - a funxtion that prints a name.
+ * @name: a pointer to the string that will be printed
+ * @f: a pointer to a the printing function
+ * Return: void.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+This directory was created to practice the use of pointers to functions in C programming language.
