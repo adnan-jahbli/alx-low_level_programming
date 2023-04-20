7c3b052dbd3d8e77833db0b092c9335dc9d560ed
adnan-jahbli <adnan.jahbli@gmail.com>
2023-04-20 16:56:28 +0000

    updating file 1 and fixing bug

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,9 +19,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%d", va_arg(args, int));
 		if ((i + 1) != n && separator)
 			printf("%s", separator);
-		else
-			printf("\n");
 	}
+	printf("\n");
 
 	va_end(args);
 }
