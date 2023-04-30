306d866a1928dc7229c66ce52418160017c7306a
adnan-jahbli <adnan.jahbli@gmail.com>
2023-04-30 21:04:32 +0000

    creating file 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - a function that frees a list and set head to NULL
+ * @head: a pointer to the first node in the list
+ *
+ * Return: void.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp = *head;
+
+	while (temp)
+	{
+		*head = (*head)->next;
+		free(temp);
+		temp = *head;
+	}
+}
