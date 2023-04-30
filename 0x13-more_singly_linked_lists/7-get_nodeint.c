62ca1d4741bc31f6186c3d8a39772f31af987f8d
adnan-jahbli <adnan.jahbli@gmail.com>
2023-04-30 23:09:06 +0000

    updating file 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -15,7 +15,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	if (!head)
 		return (NULL);
-	while (i < index)
+	while (i < index && temp != NULL)
 	{
 		temp = temp->next;
 		i++;
