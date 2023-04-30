a41c3b1e6a7b5e7f3b04072dd999881bcda173b8
adnan-jahbli <adnan.jahbli@gmail.com>
2023-04-30 21:11:44 +0000

    updating file 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,10 +10,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp = *head;
 
-	while (temp)
+	if (head)
 	{
-		*head = (*head)->next;
-		free(temp);
-		temp = *head;
+		while (temp)
+		{
+			*head = (*head)->next;
+			free(temp);
+			temp = *head;
+		}
 	}
 }
