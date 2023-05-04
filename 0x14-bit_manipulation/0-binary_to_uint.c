55ff80d8c4b3a68eedb6338a6ab49cdab9cb4305
adnan-jahbli <adnan.jahbli@gmail.com>
2023-05-04 20:44:26 +0000

    creating file 0 and updating header file

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,71 @@
+#include "main.h"
+
+int is_binary(const char *);
+unsigned int _strlen(const char *);
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: a pointer to a string of 0 and 1 chars
+ *
+ * Return: the converted number or 0 if b not containing only 1s and 0s
+ * or if b is NULL.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i, num, factor, len;
+	int *p, binary;
+
+	binary = is_binary(b);
+	if (!binary || !b)
+		return (0);
+
+	len = _strlen(b);
+	p = malloc(sizeof(int) * len);
+	if (!p)
+		return (0);
+
+	factor = 1;
+	num = 0;
+	for (i = 0; i < len; i++)
+	{
+		p[i] = b[len - 1 - i] - 48;
+		num += p[i] * factor;
+		factor *= 2;
+	}
+
+	return (num);
+}
+
+/**
+ * is_binary - checks if a string contains only 0s and 1s
+ * @b: the given string.
+ *
+ * Return: 1 if all chars are 0s and 1s and 0 otherwise.
+ */
+int is_binary(const char *b)
+{
+	unsigned int i;
+
+	for (i = 0; *(b + i); i++)
+		if (*(b + i) != '1' && *(b + i) != '0')
+			return (0);
+
+	return (1);
+}
+
+/**
+ * _strlen - counts the length of a string
+ * @s: the given string
+ *
+ * Return: number of chars in the string
+ */
+unsigned int _strlen(const char *s)
+{
+	unsigned int i;
+
+	i = 0;
+	while (s[i])
+		i++;
+
+	return (i);
+}
