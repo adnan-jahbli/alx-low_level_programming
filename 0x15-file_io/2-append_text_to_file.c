94cf3d0973f958cb752c783e65e447ead8cca9f0
adnan-jahbli <adnan.jahbli@gmail.com>
2023-05-08 23:06:37 +0000

    creating file 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - a function that creates a file
+ * @filename: the name of the file to creat
+ * @text_content: a NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	ssize_t written_bytes;
+	int fd, len;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		/* counting the length of the given string */
+		len = 0;
+		while (text_content[len])
+			len++;
+
+		/* writing the given string to the opened file */
+		written_bytes = write(fd, text_content, len);
+		if (written_bytes == -1)
+			return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
