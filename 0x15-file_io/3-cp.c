b8726b37775f2c535da9aa726684fbe9f6f99a38
adnan-jahbli <adnan.jahbli@gmail.com>
2023-05-09 01:45:48 +0000

    updating file 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,47 +12,45 @@ void error_fun(int, int, char **);
  */
 int main(int argc, char **argv)
 {
-	ssize_t read_bytes, written_bytes;
-	int fd, fd1, close_error;
-	char buffer[1024];
+    ssize_t read_bytes, written_bytes;
+    int fd, fd1, close_error;
+    char buffer[1024];
 
-	if (argc != 3)
-	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+    if (argc != 3)
+    {
+        dprintf(2, "Usage: cp file_from file_to\n");
+        exit(97);
+    }
+    fd = open(argv[1], O_RDONLY);
+    error_fun(fd, 0, argv);
 
-	fd = open(argv[1], O_RDONLY);
-	fd1 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_fun(fd, fd1, argv);
+    fd1 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+    error_fun(0, fd1, argv);
 
-	read_bytes = 1024;
-	while (read_bytes == 1024)
-	{
-		read_bytes = read(fd, buffer, 1024);
-		if (read_bytes == -1)
-			error_fun(-1, 0, argv);
+    while ((read_bytes = read(fd, buffer, 1024)) > 0)
+    {
+        written_bytes = write(fd1, buffer, read_bytes);
+        if (written_bytes == -1)
+            error_fun(0, -1, argv);
+    }
+    if (read_bytes == -1)
+        error_fun(-1, 0, argv);
 
-		written_bytes = write(fd1, buffer, read_bytes);
-		if (written_bytes == -1)
-			error_fun(0, -1, argv);
-	}
-
-	close_error = close(fd);
-	if (close_error == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
-
-	close_error = close(fd1);
-	if (close_error == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
-	return (0);
+    close_error = close(fd);
+    if (close_error == -1)
+    {
+        dprintf(2, "Error: Can't close fd %d\n", fd);
+        exit(100);
+    }
+    close_error = close(fd1);
+    if (close_error == -1)
+    {
+        dprintf(2, "Error: Can't close fd %d\n", fd1);
+        exit(100);
+    }
+    return (0);
 }
+
 /**
  * error_fun - checks for errors.
  * @file_from: file from where data will be copied
@@ -63,14 +61,14 @@ int main(int argc, char **argv)
  */
 void error_fun(int file_from, int file_to, char *argv[])
 {
-	if (file_from == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	if (file_to == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+    if (file_from == -1)
+    {
+        dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+        exit(98);
+    }
+    if (file_to == -1)
+    {
+        dprintf(2, "Error: Can't write to %s\n", argv[2]);
+        exit(99);
+    }
 }
