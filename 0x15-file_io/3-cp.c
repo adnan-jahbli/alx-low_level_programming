271c4240e70fcb7cd2ae37500e2eb6ffc48550a9
adnan-jahbli <adnan.jahbli@gmail.com>
2023-05-09 01:05:14 +0000

    updating file 3 and fixing betty

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 
+void error_fun(int, int, char **);
+
 /**
  * main - a program that copies the content of a file to another file.
  * @argc: arguments count
@@ -21,36 +23,18 @@ int main(int argc, char **argv)
 	}
 
 	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	
+	error_fun(fd, 0, argv);
+
 	read_bytes = read(fd, buffer, 1024);
 	if (read_bytes == -1)
-	{
-		close(fd);
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		error_fun(-1, 0, argv);
 
 	fd1 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd1 == -1)
-	{
-		close(fd);
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+	error_fun(0, fd1, argv);
 
 	written_bytes = write(fd1, buffer, read_bytes);
 	if (written_bytes == -1)
-	{
-		close(fd);
-		close(fd1);
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+		error_fun(0, -1, argv);
 
 	close_error = close(fd);
 	if (close_error)
@@ -66,4 +50,25 @@ int main(int argc, char **argv)
 		exit(100);
 	}
 	return (0);
+}
+/**
+ * error_fun - checks for errors.
+ * @file_from: file from where data will be copied
+ * @file_to: file where data will copied to.
+ * @argv: arguments vector.
+ *
+ * Return: void.
+ */
+void error_fun(int file_from, int file_to, char *argv[])
+{
+	if (file_from == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 }
