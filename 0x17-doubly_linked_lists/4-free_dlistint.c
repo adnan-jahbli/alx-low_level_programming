b39b51d08f7cefb155af866c88f1841ab7f9515c
adnan-jahbli <adnan.jahbli@gmail.com>
2023-06-16 02:46:25 +0000

    creating file 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Frees a dlistint_t list.
+ *
+ * @head: Head of the list.
+ *
+ * Return: No return value.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	if (head != NULL)
+		while (head->prev != NULL)
+			head = head->prev;
+
+	while ((temp = head) != NULL)
+	{
+		head = head->next;
+		free(temp);
+	}
+}
+
