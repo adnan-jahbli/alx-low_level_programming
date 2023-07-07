29c5e7e84f01800ca38f60d17e08285c0bf6812c
adnan-jahbli <adnan.jahbli@gmail.com>
2023-07-07 17:58:10 +0000

    updating file 0 with return value NULL

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,10 +13,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 
 	table = (hash_table_t *) malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
 
 	table->size = size;
 	table->array = (hash_node_t **)
-		calloc(table->size, sizeof(hash_node_t *));
+		malloc(table->size * sizeof(hash_node_t *));
+	if (table->array == NULL)
+		return (NULL);
 
 	/* initializing all the items of hash table to NULL */
 	for (i = 0; i < table->size; i++)
