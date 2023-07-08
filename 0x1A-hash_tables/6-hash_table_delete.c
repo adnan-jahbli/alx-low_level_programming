619b8d93ee97595739a4e10ab5a49100322ef32b
adnan-jahbli <adnan.jahbli@gmail.com>
2023-07-08 01:34:53 +0000

    creating file 6 and updatinf header file

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - a function that deletes a hash table.
+ * @ht: pointer to the hash table
+ *
+ * Return: void.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *temp1, *temp2;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		temp1 = ht->array[i];
+		while ((temp2 = temp1) != NULL)
+		{
+			temp1 = temp1->next;
+			free(temp2->key);
+			free(temp2->value);
+			free(temp2);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
