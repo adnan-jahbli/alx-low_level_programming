4a84c87dc21d1fb21d499dc07dc9aebcd7332858
adnan-jahbli <adnan.jahbli@gmail.com>
2023-07-08 00:51:18 +0000

    fixing error in file 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -49,7 +49,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 
-	if (ht || key || *key)
+	if (!ht || !key || !*key)
 		return (0);
 
 	index = key_index((unsigned char *) key, ht->size);
