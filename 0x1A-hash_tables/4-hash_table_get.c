9f0b9a927a2b8ca7f6b298b143a1b56db5480d7a
adnan-jahbli <adnan.jahbli@gmail.com>
2023-07-08 01:19:45 +0000

    updating file 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,15 +10,15 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int key_index;
+	unsigned long int index;
 	hash_node_t *current_node;
 
 	if (!ht || !key || !*key)
 		return (NULL);
 
-	key_index = key_index((unsigned char *) key, ht->size);
+	index = key_index((unsigned char *) key, ht->size);
 
-	current_node = ht->array[key_index];
+	current_node = ht->array[index];
 
 	while (current_node != NULL)
 	{
